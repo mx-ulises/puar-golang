23c2c5e4f060a5aaa3f5b14d05beb6806395f17d
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-08-22 23:03:08 -0700

    Chapter 8: Add Service Discovery examples of Prometheus with Service Discovery

---
+++ ch8/golang-consul-app/go.mod
@@ -0,0 +1,30 @@
+module golang-prometheus-app
+
+go 1.20
+
+require (
+	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/consul/api v1.24.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ ch8/golang-consul-app/main.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/consul/api"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	requestTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "puar_golang_http_request_total",
+			Help: "Total number of HTTP requests received in this Golang App with labels",
+		},
+		[]string{"path"},
+	)
+
+	requestError = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "puar_golang_http_request_error",
+			Help: "Number of  HTTP requests errors received in this Golang App with labels",
+		},
+		[]string{"path"},
+	)
+
+	transtactionCoinTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "puar_golang_transaction_coin_total",
+			Help: "Total number of coins transactioned",
+		},
+		[]string{"path"},
+	)
+
+	coinBalance = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "puar_golang_coin_balance",
+			Help: "Current balance of coins in this app (Saved - Spent)",
+		},
+	)
+
+	averageLatency = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "puar_golang_http_latency_seconds",
+			Help: "Average latency in seconds",
+		},
+		[]string{"path"},
+	)
+
+	percentileLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "puar_golang_http_latency_percentiles_seconds",
+			Help:    "Percentile latency in seconds",
+			Buckets: prometheus.DefBuckets, // Default linear buckets
+		},
+		[]string{"path"},
+	)
+
+	spendingAllowStatus = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "puar_golang_account_lock_status",
+			Help: "Status of the spending lock",
+		},
+		[]string{"status"},
+	)
+
+	appVersion = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "puar_golang_version",
+			Help: "App Version running",
+		},
+		[]string{"version"},
+	)
+)
+
+// Create the children to reduce lookup time:
+var (
+	// Metric children for '/v1/save' path
+	savePathLabel          = prometheus.Labels{"path": "/v1/save"}
+	saveRequestCount       = requestTotal.With(savePathLabel)
+	saveRequestErrorCount  = requestError.With(savePathLabel)
+	savedCoinCount         = transtactionCoinTotal.With(savePathLabel)
+	savedAverageLatency    = averageLatency.With(savePathLabel)
+	savedPercentileLatency = percentileLatency.With(savePathLabel)
+
+	// Metric children for '/v1/spend' path
+	spendPathLabel         = prometheus.Labels{"path": "/v1/spend"}
+	spendRequestCount      = requestTotal.With(spendPathLabel)
+	spendRequestErrorCount = requestError.With(spendPathLabel)
+	spentCoinCount         = transtactionCoinTotal.With(spendPathLabel)
+	spendAverageLatency    = averageLatency.With(spendPathLabel)
+	spendPercentileLatency = percentileLatency.With(spendPathLabel)
+
+	// Spend block status Metric
+	spendPathLockLabel   = prometheus.Labels{"status": "BLOCK"}
+	spendStatusLock      = spendingAllowStatus.With(spendPathLockLabel)
+	spendPathUnlockLabel = prometheus.Labels{"status": "UNBLOCK"}
+	spendStatusUnlock    = spendingAllowStatus.With(spendPathUnlockLabel)
+)
+
+var allowSpending = true
+
+func ConsulRegistration(consulAddress string, hostname string, port string) {
+	// Create a new Consul API client
+	config := api.DefaultConfig()
+	config.Address = consulAddress // Use the domain name and port of your Consul server
+	client, err := api.NewClient(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new service entry
+	port_int, _ := strconv.Atoi(port)
+	service := &api.AgentServiceRegistration{
+		ID:      hostname,
+		Name:    "golang-labels-app",
+		Port:    port_int,
+		Address: hostname,
+		Tags:    []string{"puar", "production"},
+		Check: &api.AgentServiceCheck{
+			HTTP:     "http://" + hostname + ":" + port + "/v1/spend",
+			Interval: "10s",
+		},
+	}
+
+	// Register the service
+	err = client.Agent().ServiceRegister(service)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Service registered with Consul")
+}
+
+func init() {
+	prometheus.MustRegister(requestTotal)
+	prometheus.MustRegister(requestError)
+	prometheus.MustRegister(transtactionCoinTotal)
+	prometheus.MustRegister(coinBalance)
+	prometheus.MustRegister(averageLatency)
+	prometheus.MustRegister(percentileLatency)
+	prometheus.MustRegister(spendingAllowStatus)
+	prometheus.MustRegister(appVersion)
+
+	appVersionLabel := prometheus.Labels{"version": os.Getenv("VERSION")}
+	appVersion.With(appVersionLabel).Set(1.0)
+	spendStatusLock.Set(0.0)
+	spendStatusUnlock.Set(1.0)
+}
+
+func GeneralHandler(
+	requestCount *prometheus.Counter,
+	requestErrorCount *prometheus.Counter,
+	coinCount *prometheus.Counter,
+	averageLatency *prometheus.Observer,
+	percentileLatency *prometheus.Observer,
+	operation string,
+	w *http.ResponseWriter,
+	r *http.Request,
+	sign float64,
+) {
+	// Get start time
+	startTime := time.Now()
+	// Increase request count
+	(*requestCount).Inc()
+
+	// Exception Handler
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Fprint(*w, "Request Error: ", err)
+			(*requestErrorCount).Inc()
+		}
+	}()
+
+	// Evaluate and increase coin count
+	queryParams := r.URL.Query()
+	coinsString := queryParams.Get("coins")
+	if coinsString == "" {
+		coinsString = "1"
+	}
+
+	coins, err := strconv.ParseFloat(coinsString, 64)
+	if err != nil {
+		panic(err)
+	}
+	if coins <= 0 {
+		panic("Negative number of coins provided")
+	}
+
+	// Evaluate Random success rate at 20%
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Float64()
+	if randomNumber < 0.2 {
+		panic("Random Error")
+	}
+
+	// Generate random sleep
+	duration := time.Duration(1000*randomNumber) * time.Millisecond
+	time.Sleep(duration)
+
+	// Increase number of coins
+	(*coinCount).Add(coins)
+	coinBalance.Add(coins * sign)
+
+	// Successful request
+	fmt.Fprint(*w, "Successful ", operation, " Request, coins: ", coins)
+	endTime := time.Now()
+	diff := float64(endTime.Sub(startTime).Seconds())
+	(*averageLatency).Observe(diff)
+	(*percentileLatency).Observe(diff)
+}
+
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+	GeneralHandler(&saveRequestCount, &saveRequestErrorCount, &savedCoinCount, &savedAverageLatency, &savedPercentileLatency, "Save", &w, r, 1)
+}
+
+func SpendHandler(w http.ResponseWriter, r *http.Request) {
+	if allowSpending {
+		GeneralHandler(&spendRequestCount, &spendRequestErrorCount, &spentCoinCount, &spendAverageLatency, &spendPercentileLatency, "Spend", &w, r, -1)
+	}
+}
+
+func LockSpending(w http.ResponseWriter, r *http.Request) {
+	allowSpending = false
+	spendStatusLock.Set(1.0)
+	spendStatusUnlock.Set(0.0)
+	fmt.Fprint(w, "Successful lock operation")
+}
+
+func UnlockSpending(w http.ResponseWriter, r *http.Request) {
+	allowSpending = true
+	spendStatusLock.Set(0.0)
+	spendStatusUnlock.Set(1.0)
+	fmt.Fprint(w, "Successful unlock operation")
+}
+
+func main() {
+	http.HandleFunc("/v1/save", SaveHandler)
+	http.HandleFunc("/v1/spend", SpendHandler)
+	http.HandleFunc("/v1/lock", LockSpending)
+	http.HandleFunc("/v1/unlock", UnlockSpending)
+	http.Handle("/metrics", promhttp.Handler())
+	port := os.Getenv("PORT")
+	hostname := os.Getenv("NAME")
+	consulAddress := "consul:8500"
+	ConsulRegistration(consulAddress, hostname, port)
+	http.ListenAndServe(":"+port, nil)
+}
