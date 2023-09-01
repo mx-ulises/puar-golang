e689e52347d2b16aa74489f2a5d37042932c0800
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-08-31 20:52:30 -0700

    Chapter 9: Add Balance Golang app and Grafana deployments

--- ch9/deploy-prometheus.sh
+++ ch9/deploy-prometheus.sh
@@ -9,7 +9,11 @@ kubectl apply -f ${KUBERNETES_SPECS_DIR}/prometheus-ClusterRoleBinding.yaml
 kubectl apply -f ${KUBERNETES_SPECS_DIR}/prometheus-config-ConfigMap.yaml
 kubectl apply -f ${KUBERNETES_SPECS_DIR}/prometheus-Deployment.yaml
 kubectl apply -f ${KUBERNETES_SPECS_DIR}/prometheus-Service.yaml
-
+kubectl apply -f ${KUBERNETES_SPECS_DIR}/grafana-Deployment.yaml
+kubectl apply -f ${KUBERNETES_SPECS_DIR}/grafana-Service.yaml
+kubectl apply -f ${KUBERNETES_SPECS_DIR}/app-Deployment.yaml
 echo "----------------------------------------"
-echo "Access Prometheus in:\n\n  - http://localhost:8001/api/v1/namespaces/prometheus/services/prometheus/proxy/\n"
+echo "Access Prometheus in:\n  - http://localhost:8001/api/v1/namespaces/prometheus/services/prometheus/proxy/\n\n"
+echo "To access Grafana run:\n  - kubectl port-forward service/grafana 3000:3000 --context kind-kind --namespace=prometheus\n"
+echo "And go to: \n  - http://localhost:3000/"
 echo "----------------------------------------"
